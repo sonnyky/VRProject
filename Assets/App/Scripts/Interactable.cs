ed7ffaa23e68688c6aed0b6d8a4d5b973245edee
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2020-08-26 00:09:19 +0900

    added candle asset
    and also light flicker effect script
    also redesigned interactable script

---
+++ Assets/App/Scripts/Interactable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    /// <summary>
+    /// Display an item (Ex: exclamation mark) to show the user this object is interactable
+    /// </summary>
+    [SerializeField]
+    GameObject m_InteractableSignPrefab;
+
+    private GameObject m_InteractableSign;
+
+    bool m_Interacting = false;
+    float m_TimeOfInteraction = 0f;
+
+    /// <summary>
+    /// Time until automatic confirmation of action
+    /// </summary>
+    [SerializeField]
+    float m_TimeUntilConfirmation;
+
+    bool m_CanConfirmAutomatically = true;
+
+    public System.Action m_Action;
+
+    public virtual void StartInteraction()
+    {
+
+    }
+
+    private void Confirmed()
+    {
+        if(m_Action != null)
+        {
+            m_Action.Invoke();
+        }
+    }
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("Pointer enter");
+        m_Interacting = true;
+    }
+
+    public void OnPointerExit()
+    {
+        StopAndResetTimer();
+    }
+
+    private void StopAndResetTimer()
+    {
+        m_Interacting = false;
+        m_TimeOfInteraction = 0f;
+    }
+
+    private void Update()
+    {
+        if (m_Interacting)
+        {
+            TimerUntilConfirmation();
+            if(m_TimeOfInteraction > m_TimeUntilConfirmation)
+            {
+                Confirmed();
+                StopAndResetTimer();
+            }
+        }
+    }
+
+    public virtual void TimerUntilConfirmation()
+    {
+        m_TimeOfInteraction += Time.deltaTime;
+    }
+}
--- Assets/App/Scripts/SwitchBox.cs
+++ Assets/App/Scripts/SwitchBox.cs
@@ -1,127 +1,20 @@
-﻿// Copyright 2014 Google Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
-//This script is based on Google's sample so I'm leaving the above licensing.
-
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class SwitchBox : MonoBehaviour
+public class SwitchBox : Interactable
 {
-    private Vector3 startingPosition;
-    //Properties to handle auto select, based on how long the user gazes at the object
-    private float countdownToAutoConfirm;
-    private float waitTimeUntilAutoConfirm = 2.0f;
-    private bool waitingConfirmationFlag;
 
-    //Properties to handle icon display to indicate user is interacting with the object
-    private GameObject exclamationMark, exclamationMarkInstance;
-    private Vector3 deltaPos, scale;
-
-    // Properties to handle user picking an object
-    private CameraMovement playerBody; //Class to interact with the player, calling methods in CameraMovement.cs
-
-    //Unused right now
-    private Quaternion markQuarternion;
-
-    //Parameters to control the lights
     [SerializeField]
     GameObject m_Lights;
-    
-    void Start()
+
+    private void Start()
     {
-        toggleBedroomLights(false);
-        scale = new Vector3(0.1f, 0.1f, 0.1f);
-        markQuarternion = new Quaternion();
-        startingPosition = transform.localPosition;
-        deltaPos.x = startingPosition.x;
-        deltaPos.y = startingPosition.y + 0.12f;
-        deltaPos.z = startingPosition.z;
-
-        exclamationMark = Resources.Load("ExclamationMark") as GameObject;
-
-        waitingConfirmationFlag = false;
-        countdownToAutoConfirm = waitTimeUntilAutoConfirm;
-        SetGazedAt(false);
+        m_Action += SwitchLights;
     }
 
-    void LateUpdate()
+    public void SwitchLights()
     {
-        //check every frame if the object is being gazed upon
-
-
-        if (waitingConfirmationFlag)
-        {
-
-            countdownToAutoConfirm -= Time.deltaTime;
-            if (countdownToAutoConfirm < 0)
-            {
-                //print("gazing more than 2 seconds");
-
-                //This part for bedroom lights
-                toggleBedroomLights(true);
-                playerBody.changeAudioSource(5);
-                SetGazedAt(false);
-               // Destroy(gameObject);
-            }
-
-        }
-
-       
+        Debug.Log("Lights switched");
     }
-
-    public void SetGazedAt(bool gazedAt)
-    {
-        //GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
-
-        if (gazedAt)
-        {
-            print("Looking at object");
-            exclamationMarkInstance = GameObject.Instantiate(exclamationMark, deltaPos, markQuarternion) as GameObject;
-            exclamationMarkInstance.name = "exclamationMarkInstance";
-            exclamationMarkInstance.transform.localScale = scale;
-
-            waitingConfirmationFlag = true;
-            //print(waitingConfirmationFlag);
-        }
-        if (!gazedAt)
-        {
-            //print("Not Looking at object");
-            waitingConfirmationFlag = false;
-            Destroy(GameObject.Find("exclamationMarkInstance"));
-            countdownToAutoConfirm = waitTimeUntilAutoConfirm;
-        }
-    }
-
-    public void Reset()
-    {
-        transform.localPosition = startingPosition;
-    }
-
-    public void TeleportRandomly()
-    {
-        Vector3 direction = Random.onUnitSphere;
-        direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-        float distance = 2 * Random.value + 1.5f;
-        transform.localPosition = direction * distance;
-    }
-
-    void toggleBedroomLights(bool switchLights)
-    {
-      
-    }
+ 
 }
